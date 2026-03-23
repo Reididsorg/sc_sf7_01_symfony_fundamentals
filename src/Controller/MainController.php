d5dc42509fadaa7ce7859b53c3728c57f70e64d4
bruno.grosdidier@2dcom.fr <bruno.grosdidier@2dcom.fr>
2026-03-23 09:51:14 +0100

    Ajout d'un cache pour accélérer le temps de réponse lié  la requete http  l'API ISS de la page d'accueil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,18 +6,28 @@ use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class MainController extends AbstractController
 {
     #[Route('/', name: 'app_homepage')]
-    public function homepage(StarshipRepository $starshipRepository, HttpClientInterface $client): Response
+    public function homepage(StarshipRepository $starshipRepository, HttpClientInterface $client, CacheInterface $cache): Response
     {
         $ships = $starshipRepository->findAll();
         $myShip = $ships[array_rand($ships)];
 
-        $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
-        $issData = $response->toArray();
+        $issData = $cache->get('iss_location_data', function (ItemInterface $item) use ($client): array {
+            // Le cache 'iss_location_data' situé dans var/cache/dev/pools/app expire après 200 secondes.
+            $item->expiresAfter(200);
+
+            // La requete http n'est effectuée que si le cache ('iss_location_data' n'existe pas ou est expiré.
+            $response = $client->request('GET', 'https://api.wheretheiss.at/v1/satellites/25544');
+
+            // Si un cache est récupéré, la réponse en cache est directement renvoyée.
+            return $response->toArray();
+        });
 
         return $this->render('main/homepage.html.twig', [
             'myShip' => $myShip,
